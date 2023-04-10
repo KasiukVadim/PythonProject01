0e5374b9a5e49e3b6c3c7532f7e495cb44e1585d
Kasiuk Vadim <92652010+KasiukVadim@users.noreply.github.com>
2023-04-10 04:55:54 +0300

    Rewrite from base
    Переписал все с нуля, потому что пролая версия была совсем никудышной. Сделал дизайн +-, табличку рекордов и босувы во время игры(добавил 2 вида яблок черное и золотое). Уровни сложности пока не добавил

--- main.py
+++ main.py
@@ -1,50 +1,172 @@
-import time
 import pygame
+import sys
 import random
+import pygame_menu
 
 pygame.init()
-screen_d = 900
-screen_h = 600
-screen = pygame.display.set_mode((screen_d, screen_h))
-pygame.display.set_caption("MyFirstGame - Snake")
-icon = pygame.image.load('4591880_animal_carnivore_cartoon_fauna_snake_icon.png')
-pygame.display.set_icon(icon)
+bg_image = pygame.image.load('logo.jpg')
+YELLOW = (204, 204, 0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+FRAME_COLOR = (0, 255, 204)
+WHITE = (255, 255, 255)
+BLUE = (204, 255, 255)
+SIZE_BLOCK = 20
+COUNT_BLOCKS = 20
+MARGIN = 1
+HEADER_COLOR = (0, 204, 153)
+HEADER_MARGIN = 70
+SNAKE_COLOR = (0, 102, 0)
+timer = pygame.time.Clock()
 
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
+size = [SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS,
+        SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS + HEADER_MARGIN]
 
-myfont = pygame.font.Font('Teko-Bold.ttf', 40)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Змейка')
+courier = pygame.font.SysFont('courier', 36)
 
-def message(text, color):
-    screen.blit(myfont.render(text, False, color), (screen_d // 2 - 100, screen_h // 2 - 50))
 
-def FoodGen():
-    f_x = round(random.randrange(0, screen_d - snake_size) / snake_size) * snake_size
-    f_y = round(random.randrange(0, screen_h - snake_size) / snake_size) * snake_size
-    return (f_x, f_y)
+def draw_block(color, row, column):
+    pygame.draw.rect(screen, color, [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
+                                     HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1),
+                                     SIZE_BLOCK,
+                                     SIZE_BLOCK])
 
-def snake_body(snake_size, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(screen, snake_col, (x[0], x[1], snake_size, snake_size))
 
-def Check(snake_list):
-    if snake_list[0][0] >= screen_d or snake_list[0][0] < 0 or snake_list[0][1] >= screen_h or snake_list[0][1] < 0:
-        return True
-    for i in range(1, len(snake_list)):
-        if snake_list[i] == snake_list[0] :
-            return True
-    return False
+def get_random_empty_block():
+    x = random.randint(0, COUNT_BLOCKS - 1)
+    y = random.randint(0, COUNT_BLOCKS - 1)
+    empty_block = SnakeBlock(x, y)
+    while empty_block in snake_blocks:
+        x = random.randint(0, COUNT_BLOCKS - 1)
+        y = random.randint(0, COUNT_BLOCKS - 1)
+        empty_block = SnakeBlock(x, y)
+    return empty_block
 
-snake_size = 30
-score = 0
-records = []
 
-def PrintRecords():
-    records.sort(reverse=True)
-    for i in range(min(len(records), 5)):
-        screen.blit(myfont.render("{}. {} {}".format(i + 1, records[i][1], records[i][0]), False, red), (0, 50 * i))
-    screen.blit(myfont.render("Press Q to return", False, red), (0, 50 * 6))
+class SnakeBlock:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def is_inside(self):
+        return 0 <= self.x < SIZE_BLOCK and 0 <= self.y < SIZE_BLOCK
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+snake_blocks = [SnakeBlock(COUNT_BLOCKS // 2, COUNT_BLOCKS // 2)]
+
+RECORDS = []
+
+
+def start_the_game():
+    d_row, d_col = 1, 0
+    apple = get_random_empty_block()
+    golden_apple = get_random_empty_block()
+    black_apple = get_random_empty_block()
+    total = 0
+    speed = 0
+    snake_blocks = [SnakeBlock(COUNT_BLOCKS // 2, COUNT_BLOCKS // 2)]
+    golden_apple_flag = False
+    black_apple_flag = False
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print('exit')
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                print('KEY')
+                if event.key == pygame.K_UP and d_row != 1:
+                    d_row, d_col = -1, 0
+                elif event.key == pygame.K_DOWN and d_row != -1:
+                    d_row, d_col = 1, 0
+                elif event.key == pygame.K_LEFT and d_col != 1:
+                    d_row, d_col = 0, -1
+                elif event.key == pygame.K_RIGHT and d_col != 1:
+                    d_row, d_col = 0, 1
+
+        screen.fill(FRAME_COLOR)
+        pygame.draw.rect(screen, HEADER_COLOR, [0, 0, size[0], HEADER_MARGIN])
+        text_total = courier.render(f"Total: {total}", False, WHITE)
+        text_speed = courier.render(f"Speed: {speed}", False, WHITE)
+        screen.blit(text_total, (SIZE_BLOCK, SIZE_BLOCK))
+        screen.blit(text_speed, (SIZE_BLOCK + 200, SIZE_BLOCK))
+        for row in range(COUNT_BLOCKS):
+            for column in range(COUNT_BLOCKS):
+                if (column + row) % 2 == 0:
+                    colour = BLUE
+                else:
+                    colour = WHITE
+                draw_block(colour, row, column)
+
+        head = snake_blocks[-1]
+        if not head.is_inside():
+            print('crash')
+            RECORDS.append([total, name.get_value()])
+            break
+        draw_block(RED, apple.x, apple.y)
+        if golden_apple_flag:
+            draw_block(YELLOW, golden_apple.x, golden_apple.y)
+        if black_apple_flag:
+            draw_block(BLACK, black_apple.x, black_apple.y)
+        for block in snake_blocks:
+            draw_block(SNAKE_COLOR, block.x, block.y)
+        if apple == head:
+            print("eat")
+            total += 1
+            speed = abs(total) // 5
+            snake_blocks.append(apple)
+            apple = get_random_empty_block()
+            if random.randint(0, 5) == 1:
+                golden_apple_flag = True
+            if random.randint(0, 5) == 1:
+                black_apple_flag = True
+        if golden_apple == head:
+            print("eat")
+            total += 3
+            speed = abs(total) // 5
+            snake_blocks.append(golden_apple)
+            golden_apple_flag = False
+            if random.randint(0, 5) == 1:
+                golden_apple_flag = True
+            if random.randint(0, 5) == 1:
+                black_apple_flag = True
+        if black_apple == head:
+            print("eat")
+            total -= 5
+            speed = abs(total) // 5
+            snake_blocks.append(black_apple)
+            black_apple_flag = False
+            if random.randint(0, 5) == 1:
+                golden_apple_flag = True
+            if random.randint(0, 5) == 1:
+                black_apple_flag = True
+        new_head = SnakeBlock(head.x + d_row, head.y + d_col)
+        if new_head in snake_blocks:
+            print('crash')
+            RECORDS.append([total, name.get_value()])
+            break
+        snake_blocks.append(new_head)
+        snake_blocks.pop(0)
+
+        pygame.display.flip()
+        timer.tick(3 + speed)
+
+
+def records_table():
+    screen.fill(FRAME_COLOR)
+    RECORDS.sort(reverse=True)
+    screen.blit(courier.render("№  Name   Score", False, WHITE), (SIZE_BLOCK, SIZE_BLOCK))
+    for rec in range(min(len(RECORDS), 5)):
+        text = courier.render("{}. {} {}".format(rec + 1, RECORDS[rec][1], RECORDS[rec][0]), False, WHITE)
+        screen.blit(text, (SIZE_BLOCK, SIZE_BLOCK + 50 * (rec + 1)))
+    screen.blit(courier.render("Press Q to return", False, WHITE), (0, 50 * 8))
     pygame.display.update()
     while True:
         for event in pygame.event.get():
@@ -55,130 +177,26 @@ def PrintRecords():
                 if event.key == pygame.K_q:
                     return
 
-def GameLoop(snake_col, level, name):
-    snake_speed = 5
-    square = pygame.Surface((snake_size, snake_size))
-    square.fill(snake_col)
-    x0, y0 = screen_d // 2, screen_h // 2
-    dx, dy = 0, 0
-    clock = pygame.time.Clock()
 
-    snake = [[x0, y0]]
-    fx, fy = -1, -1
-    score = 0
-    food_flag = False
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                exit(0)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    if dx == snake_size:
-                        continue
-                    dx = -snake_size
-                    dy = 0
-                elif event.key == pygame.K_RIGHT:
-                    if dx == -snake_size:
-                        continue
-                    dx = snake_size
-                    dy = 0
-                elif event.key == pygame.K_UP:
-                    if dy == snake_size:
-                        continue
-                    dx = 0
-                    dy = -snake_size
-                elif event.key == pygame.K_DOWN:
-                    if dy == -snake_size:
-                        continue
-                    dx = 0
-                    dy = snake_size
-                elif event.key == pygame.K_SPACE:
-                    dx = 0
-                    dy = 0
-        x0 += dx
-        y0 += dy
-        snake.insert(0, [x0, y0])
-        snake.pop(-1)
-        if Check(snake):
-            running = False
-            screen.fill('black')
-            message("You are lost!", red)
-            screen.blit(myfont.render("Your score is {}!".format(score), False, red),
-                        (screen_d // 2 - 100, screen_h // 2))
-            pygame.display.update()
-            time.sleep(3)
-            records.append([score, name])
-            break
-        if not food_flag:
-            fx, fy = FoodGen()
-            food_flag = True
-        if x0 == fx and y0 == fy and food_flag:
-            score += 1
-            food_flag = False
-            if score == 1:
-                snake.append([snake[0][0] - dx, snake[0][1] - dy])
-            else:
-                snake.append([snake[-1][0] - (snake[-2][0] - snake[-1][0]), snake[-1][1] - (snake[-2][1] - snake[-1][1])])
-        screen.fill('black')
-        pygame.draw.rect(screen, 'RED', (fx, fy, snake_size, snake_size))
-        snake_body(snake_size, snake)
-        pygame.display.update()
-        clock.tick(snake_speed + score / 3)
+menu = pygame_menu.Menu('Welcome', 400, 300,
+                        theme=pygame_menu.themes.THEME_BLUE)
 
+name = menu.add.text_input('Имя:', default='Игрок 1')
+menu.add.button('Рекорды', records_table)
+menu.add.button('Играть', start_the_game)
+menu.add.button('Выход', pygame_menu.events.EXIT)
 
 while True:
-    screen.fill('black')
-    message("Hello!", red)
-    screen.blit(myfont.render("1-Play 2-Records", False, red), (screen_d // 2 - 100, screen_h // 2))
+
+    screen.blit(bg_image, (0, 0))
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            exit()
+
+    if menu.is_enabled():
+        menu.update(events)
+        menu.draw(screen)
+
     pygame.display.update()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
-                exit(0)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_1:
-                    screen.fill('black')
-                    message("Enter your name!!", red)
-                    pygame.display.update()
-                    name = ''
-                    while event.key != pygame.K_RETURN:
-                        pygame.event.clear()
-                        pygame.event.wait()
-                        name += event.unicode
-                        if len(name) > 9:
-                            break
-                    screen.fill('black')
-                    message("Select color of snake!", red)
-                    screen.blit(myfont.render("1-Blue 2-Yellow 3-Green", False, red),
-                                (screen_d // 2 - 100, screen_h // 2))
-                    pygame.display.update()
-                    snake_col = ''
-                    while True:
-                        for event in pygame.event.get():
-                            if event.type == pygame.QUIT:
-                                running = False
-                                pygame.quit()
-                                exit(0)
-                            if event.type == pygame.KEYDOWN:
-                                if event.key == pygame.K_1:
-                                    snake_col = 'Blue'
-                                    break
-                                elif event.key == pygame.K_2:
-                                    snake_col = 'Yellow'
-                                    break
-                                elif event.key == pygame.K_3:
-                                    snake_col = 'Green'
-                                    break
-                        if snake_col != '':
-                            break
-                    GameLoop(snake_col, 1, name)
-                    break
-                elif event.key == pygame.K_2:
-                    screen.fill('black')
-                    PrintRecords()
-        break
